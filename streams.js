fc98db02d435ac3589a609ca23e34a5f718a80fa
ShawLevin <shawlevin@gmail.com>
2016-02-14 16:27:01 -0500

    now we have a server!

--- streams.js
+++ streams.js
@@ -1,14 +1,29 @@
-var fs = require('fs');
+var fs = require('fs'); //File Streams
+var zlib = require('zlib'); //Zip
 
 var readable = fs.createReadStream(__dirname + '/content/lorem.txt',
 {
-    encoding:'utf8', highWaterMark: 8*1024
+    encoding:'utf8',
+    highWaterMark: 8*1024 //Override the default buffer size so we can see chunking.
 })
 
-var writable = fs.createWriteStream(__dirname+'/content/lorem_copy.txt');
+var writable = fs.createWriteStream(__dirname+'/content/lorem_piped.txt');
 
-readable.on('data', function(chunk)
-{
-    console.log(chunk.length);
-    writable.write(chunk);
-})
+/*
+//Ditch this it is built in to node - see pipe below.
+readable.on('data', //looks like the event emitter looking for the data event.
+    function(chunk)
+    {
+        console.log(chunk.length);
+        writable.write(chunk);
+    }
+)*/
+
+readable.pipe(writable); //We can use pipe instead of making our own data writing event.
+
+var compressed = fs.createWriteStream(__dirname+'/content/lorem_zipped.txt.gz');
+var gzip = zlib.createGzip(); //Transform stream so we can compress the data.
+
+//First pipe to zip the data then pipe to the gz writter. 
+//Compressed write isn't doing any compression. It is just writing the compressed bits.
+readable.pipe(gzip).pipe(compressed); 
